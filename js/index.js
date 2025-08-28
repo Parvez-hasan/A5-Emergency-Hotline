6f96320aeb532621a101b3a54484e19fcc62c1c3
Parvez-hasan <parvez7788@gmail.com>
2025-08-29 03:00:06 +0600

    coin count add

--- js/index.js
+++ js/index.js
@@ -9,10 +9,103 @@ function heardAdd (){
     
 };
 
+//document.getElementById("call-button").addEventListener('click', function(){
+
+//    alert("call button click")
+//})
+
 // Call Buttons //
 
+   //function call button section
 
-document.getElementById("call-button").addEventListener('click', function(){
+    const allCalls = [];
+    
+    function  btnClick (btnAdd, headTitle, numberAdd){
 
-    alert("call button click")
-})
+        document.getElementById(btnAdd).addEventListener("click", function(){
+
+            const coinGrow = document.getElementById("coin-gro").innerText
+
+            const headTi = document.getElementById(headTitle).innerText
+            const impNumber = document.getElementById(numberAdd).innerText
+
+        if(coinGrow < 20){
+            alert ("❌You do not have enough coins. You need at least 20 coins to make a call")
+            return;
+
+        }
+        document.getElementById("coin-gro").innerText = coinGrow - 20
+         alert(`📞calling ${title} ${numberEmar}...`)
+
+         const callHistory ={
+            callName : title,
+            number : numberEmar,
+            time : new Date().toLocaleTimeString()
+
+         }
+
+         allCall.push(callHistory);
+
+      const allCallHistory = document.getElementById("call-history")
+      allCallHistory.innerText = " "
+
+      for(const callHistory of allCalls){
+        const div = document.createElement('div')
+        div.innerText = `
+            <div class="bg-[#f5fff6] flex justify-between items-center rounded-lg p-4">
+                 <div>
+                   <h2 class="font-semibold">${callHistory.callName}</h2>
+                   <h4 class="text-gray-700">${callHistory.number}</h4>
+                 </div>
+
+                 <h2>${callHistory.time}</h2>
+        
+           </div> `
+           allCallHistory.appendChild(div)
+      }
+
+        })
+    };
+
+    // all contact call button //
+      
+    // National hotline
+    btnClick("national-hotline-btn", "national-hotline", "national-hotline-number");
+
+    //Police hotline
+    btnClick("police-hotline-btn", "police-hotline", "police-hotline-number");
+
+    //fire hotline
+    btnClick("fire-hotline-btn", "fire-hotline", "fire-hotline-number");
+
+    //ambulance hotline
+    btnClick("ambulance-hotline-btn", "ambulance-hotline", "ambulance-hotline-number");
+
+    //women hotline
+    btnClick("women-hotline-btn", "women-hotline", "women-hotline-number");
+
+    // anti hotline
+    btnClick("anti-hotline-btn", "anti-hotline", "anti-hotline-number")
+
+    // electric hotline
+    btnClick("electric-hotline-btn", "electric-hotline", "electric-hotline-number")
+
+    //brac hotline
+    btnClick("brac-hotline-btn", "brac-hotline", "brac-hotline-number");
+
+    //railway hotline
+    btnClick("railway-hotline-btn", "railway-hotline", "railway-hotline-number")
+
+//Clear Button
+ document.getElementById("clear-button").addEventListener("click", function(){
+      
+    document.getElementById("call-history").innerText = ""
+    allCalls.length = 0;
+
+
+ });
+
+
+
+
+
